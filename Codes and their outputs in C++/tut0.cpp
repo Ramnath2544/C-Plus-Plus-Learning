3cd613045246908a357465c59a987e04aca8c3f6
Ramnath <ramnath2544@gmail.com>
2025-11-19 16:56:59 +0530

    Increment and Decrement Operator

--- Codes and their outputs in C++/tut0.cpp
+++ Codes and their outputs in C++/tut0.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello world";
+    int x = 5, y = 7;
+    cout<<x++<<" "<<++y<<" "<<x<<" "<<y;
+    // cout<<"Enter the First Number: \n";
+    // cin>>a;
+    // cout<<"Enter the Second Number: \n";
+    // cin>>b;
+    // if(b == 0){
+    //     cout<<"Error! b can't be zero";
+    // } else {
+    //     result = a/b;
+    //     cout<<"The division of a and b:\n "<<result<<"\n";
+    // }
     return 0;
 }
