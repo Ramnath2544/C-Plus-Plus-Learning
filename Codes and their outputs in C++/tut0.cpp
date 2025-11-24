3b628265d5e07517da39db3b1449b9dba5720b32
Ramnath <ramnath2544@gmail.com>
2025-11-24 17:31:35 +0530

    Restore previous one

--- Codes and their outputs in C++/tut0.cpp
+++ Codes and their outputs in C++/tut0.cpp
@@ -1,17 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x = 5, y = 7;
-    cout<<x++<<" "<<++y<<" "<<x<<" "<<y;
-    // cout<<"Enter the First Number: \n";
-    // cin>>a;
-    // cout<<"Enter the Second Number: \n";
-    // cin>>b;
-    // if(b == 0){
-    //     cout<<"Error! b can't be zero";
-    // } else {
-    //     result = a/b;
-    //     cout<<"The division of a and b:\n "<<result<<"\n";
-    // }
-    return 0;
+   cout<<"Hello World";
+   return 0;
 }
