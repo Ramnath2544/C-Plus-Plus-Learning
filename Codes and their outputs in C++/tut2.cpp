de7147ab95656053a183a61c584d130eb940fc95
Ramnath <ramnath2544@gmail.com>
2025-11-27 11:20:23 +0530

    Add a prime number program

---
+++ Codes and their outputs in C++/tut2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i, flag = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n <= 1)
+    {
+        cout << "Not a prime number";
+        return 0;
+    }
+
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+    {
+        cout << "Prime Number";
+    }
+    else
+    {
+        cout << "Not a prime number";
+    }
+
+    return 0;
+}
